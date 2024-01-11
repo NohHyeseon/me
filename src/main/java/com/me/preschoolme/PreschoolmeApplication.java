652f95e6fe42032b13c9d3268c90ca55a4a9b843
sunxno <sunxno@naver.com>
2024-01-11 18:09:08 +0900

    2020

---
+++ src/main/java/com/me/preschoolme/PreschoolmeApplication.java
@@ -0,0 +1,13 @@
+package com.me.preschoolme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreschoolmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreschoolmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/me/preschoolme/PreschoolmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.preschoolme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreschoolmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
