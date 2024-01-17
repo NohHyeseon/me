4753e2eaeee9eb770022777bb47198fef90a9171
sunxno <sunxno@naver.com>
2024-01-17 18:06:17 +0900

    2024

--- src/main/java/com/me/preschoolme/parent/ParentController.java
+++ src/main/java/com/me/preschoolme/parent/ParentController.java
@@ -2,6 +2,8 @@ package com.me.preschoolme.parent;
 
 import com.me.preschoolme.common.ResVo;
 import com.me.preschoolme.parent.model.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +12,16 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/parent")
+@Tag(name = "부모님", description = "부모님 등록 삭제 외 수정, 아이 연결 ")
 public class ParentController {
     private final ParentService service;
 
     //식별코드매칭
     @GetMapping("/check")
+    @Operation(summary = "식별코드 체크", description = "<strong>식별코드 체크</strong><br><br>" +
+            "식별코드로 회원가입 전 검증단계<br>" +
+            "성공시 원아 정보응답<br>" +
+            "실패시 에러메세지송출 <br>")
     public CodeVo postParentCheck(@RequestBody CodeDto dto) {
         return service.getMatch(dto);
     }
@@ -22,18 +29,30 @@ public class ParentController {
 
     //부모아이디 중복확인
     @GetMapping("/signup")
+    @Operation(summary = "아이디 중복체크", description = "<strong>아이디 중복 체크</strong><br><br>" +
+            "회원가입 전 아이디 중복체크<br>" +
+            "성공시 1로 응답<br>" +
+            "실패시 에러메세지송출 <br>")
     public ResVo postParentSignup(ParentInsDto dto) {
         return service.chekUid(dto);
     }
 
     //부모회원가입
     @PostMapping("/signup")
+    @Operation(summary = "부모님 회원가입", description = "<strong>부모님 회원가입</strong><br><br>" +
+            "이상없을 경우 회원가입 성공<br>" +
+            "성공시 1로 응답<br>" +
+            "실패시 에러메세지송출 <br>")
     public ResVo postSignup(@RequestBody ParentInsDto dto) {
         return service.insParent(dto);
     }
 
     //부모님 로그인
     @PostMapping("/signin")
+    @Operation(summary = "로그인", description = "<strong>부모님 로그인</strong><br><br>" +
+            "uid와 upw로 로그인<br>" +
+            "성공시 아이PK, 부모 PK 응답<br>" +
+            "실패시 에러메세지송출 <br>")
     public ParentKid postParentSignin(@RequestBody ParentSigninDto dto){
         return service.parentSignin(dto);
     }
